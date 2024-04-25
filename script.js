568bc18a50c4af0e164f1d0be47130ad2f76cd2e
Campbellcst <157153698+Campbellcst@users.noreply.github.com>
2024-04-24 19:13:50 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,32 +2,60 @@ const loginForm = document.getElementById("login-form");
 const userInputName = document.getElementById("username");
 const userInputPassword = document.getElementById("password");
 const submitBtn = document.getElementById("submit-btn");
+const messageDisplay = document.getElementById("message");
+const giftYes = document.getElementById('gift-yes');
+const giftNo = document.getElementById('gift-no');
+const giftMessage = document.getElementById('gift-message');
 const messageDisplayHome = document.getElementById("message-home");
 const messageDisplayLogin = document.getElementById("message-login");
 
 const validUsers = [
-  { username: "manager", password: "manager123" },
-  { username: "member", password: "member456" },
-];
+    { username: "manager", password: "manager123" },
+    { username: "member", password: "member456" },
+  ];
+  
+  const checkValidUser = () => {
+    const foundUser = validUsers.find(
+      (user) =>
+        user.username === userInputName.value &&
+        user.password === userInputPassword.value
+    );
+    if (foundUser) {
+      const isManager = foundUser.username === "manager";
+      localStorage.setItem("isManager", isManager);
+      window.location.href = "home.html";
+    } else {
+      messageDisplayLogin.innerHTML = "Invalid username or password.";
+    }
+  };
+  
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkValidUser();
+    userInputName.value = "";
+    userInputPassword.value = "";
+  });
 
-const checkValidUser = () => {
-  const foundUser = validUsers.find(
-    (user) =>
-      user.username === userInputName.value &&
-      user.password === userInputPassword.value
-  );
-  if (foundUser) {
-    const isManager = foundUser.username === "manager";
-    localStorage.setItem("isManager", isManager);
-    window.location.href = "home.html";
-  } else {
-    messageDisplayLogin.innerHTML = "Invalid username or password.";
-  }
-};
+function toggleEdit() {
+    var textElements = document.querySelectorAll('#fields span');
+    textElements.forEach((element) => {
+        var isEditable = element.getAttribute('contenteditable') === 'true';
+        element.setAttribute('contenteditable', !isEditable);
+        var saveChangesBtn = document.getElementById('save-changes');
+        saveChangesBtn.style.display = 'block';
+    });
+}
 
-loginForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkValidUser();
-  userInputName.value = "";
-  userInputPassword.value = "";
-});
+function saveChanges() {
+    var textElements = document.querySelectorAll('#fields span');
+    textElements.forEach((element) => {
+        var id = element.id;
+        var newValue = element.textContent;
+        document.getElementById(id).textContent = newValue;
+        element.setAttribute('contenteditable', 'false');
+        var saveChangesBtn = document.getElementById('save-changes');
+        saveChangesBtn.style.display = 'none';
+    });
+}
+
+
